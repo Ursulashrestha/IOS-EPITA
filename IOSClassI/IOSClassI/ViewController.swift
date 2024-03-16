7c85c38f5671f718b6471127ea2cc30c08ec64dd
Ursula <scrappermoon1999@gmail.com>
2024-03-16 21:57:31 +0100

    Initial UI base

---
+++ IOSClassI/IOSClassI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSClassI
+//
+//  Created by Guest User on 12/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstlabel: UILabel!
+    @IBOutlet weak var secondlqbel: UILabel!
+    @IBOutlet weak var thirdlqbel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstlabel.text = "Hello World"
+        secondlqbel.text = "fnjrbfjkrdbjkr  frkbjkrbfjrkf bgjrbgfjskrbskfs nfsjkrbfjskfsrf nfjsknsfkfksfs nfsjkebfsjkdfsdjkf bfsjkdbfsdjkfsb nfjfkdbnsfjkbksf njdfsdkfskfsjldfs"
+        thirdlqbel.text = "fnjrbfjkrdbjkr  frkbjkrbfjrkf bgjrbgfjskrbskfs nfsjkrbfjskfsrf nfjsknsfkfksfs nfsjkebfsjkdfsdjkf bfsjkdbfsdjkfsb nfjfkdbnsfjkbksf njdfsdkfskfsjldfs"
+    }
+
+
+    
+}
+
